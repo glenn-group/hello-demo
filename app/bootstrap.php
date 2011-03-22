19ff0b54acf834e4540d85ac4ad1ce4be20bf8f2
Jens Ljungblad <jens.ljungblad@gmail.com>
2011-03-22 15:18:31 +0100

    updated bootstrap and framework

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -10,27 +10,8 @@ if (!Loader::isRegistered('glenn')) {
 	throw new \Exception('blah');
 }
 
-
-
-
 $request = new Request();
 $router = new \glenn\router\RouterTree();
-
-//$params = array($request);
-
-
-//$frontController = new FrontController($router);
-
-$frontController = new \ReflectionClass('\glenn\http\Request');
-$frontController = $frontController->newInstanceArgs(array($router));
-
-//print_r($frontController);
-
-//exit;
-
-//$frontController = new FrontController($router);
-
-//exit;
-
+$frontController = new FrontController($router);
 $response = $frontController->dispatch($request);
 $response->send();
